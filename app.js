376885ab157f9c59560bbe3bf5b18bc9ffb9866d
henryscholl <henry.scholl@gmail.com>
2017-04-11 19:21:08 +0000

    rename main view

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", function(req, res) {
-   res.render("home"); 
+   res.render("animations"); 
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
